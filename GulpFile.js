21a98a3ae94c8a6fa2f3c599def3b85c35225e9e
Bryan Robinson <bryanlrobinson@gmail.com>
2016-12-19 12:00:07 -0600

    HOTFIX for gulp

--- GulpFile.js
+++ GulpFile.js
@@ -2,7 +2,6 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
@@ -13,9 +12,7 @@ gulp.task('default', ['sass', 'sass:watch']);
 
 gulp.task('sass', function() {
    return gulp.src('scss/**/*.scss')
-       .pipe(sourcemaps.init())
        .pipe(sass().on('error', sass.logError))
-       .pipe(sourcemaps.write())
        .pipe(autoprefixer())
        .pipe(gulp.dest('./'))
 });
